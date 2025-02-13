4a760c47b7c219194923086018c1c9bf345d5d5c
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-02-13 16:17:16 -0600

    metas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,48 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "El hombre en busca del sentido",
+  title: "El Hombre en Busca del Sentido - Compra con Descuento | LAZOX",
   description:
-    "Compra tu libro con un super descuento y gana más beneficios por afiliarte a LAZOX",
+    "Consigue 'El Hombre en Busca del Sentido' con un gran descuento. Únete a LAZOX y obtén beneficios exclusivos.",
+  keywords: [
+    "El Hombre en Busca del Sentido",
+    "Libro Viktor Frankl",
+    "Comprar libros con descuento",
+    "Beneficios LAZOX",
+  ],
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://libro.lazox.com.mx/",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://libro.lazox.com.mx/",
+    title: "El Hombre en Busca del Sentido - Compra con Descuento | LAZOX",
+    description:
+      "Consigue 'El Hombre en Busca del Sentido' con un gran descuento. Únete a LAZOX y obtén beneficios exclusivos.",
+    images: [
+      {
+        url: "/lazox-meta-logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Portada del libro El Hombre en Busca del Sentido",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "El Hombre en Busca del Sentido - Compra con Descuento | LAZOX",
+    description:
+      "Consigue 'El Hombre en Busca del Sentido' con un gran descuento. Únete a LAZOX y obtén beneficios exclusivos.",
+    images: ["/lazox-meta-logo.jpg"],
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${montserrat.variable} antialiased`}>{children}</body>
     </html>
   );
