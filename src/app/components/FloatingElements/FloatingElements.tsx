c705def292da6e435f275015e3b4d1bb2686ccf2
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-02-17 17:16:49 -0600

    fix lint

--- src/app/components/FloatingElements/FloatingElements.tsx
+++ src/app/components/FloatingElements/FloatingElements.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import react, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ArrowUp } from "lucide-react";
 import whatsapp from "../../../../public/whatsapp.svg";
 
